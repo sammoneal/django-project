d17f6a0bbbbcd038f6be41a453b61aa4ea612828
Sam O'Neal <sam.oneal@gmail.com>
2024-03-05 18:12:45 -0800

    Adds class based view, function based view, and static image file.

--- myapp/urls.py
+++ myapp/urls.py
@@ -4,4 +4,6 @@ from . import views
 urlpatterns = [
     path("", views.home, name="home"),
     path("about", views.about, name="about"),
+    path('function/', views.function_view, name='function_view'),
+    path('class/', views.ClassView.as_view(), name='class_view'),
 ]
