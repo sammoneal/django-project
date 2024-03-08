7e3c50880e88ca4444f32267e6f8bc49c579761b
Sam O'Neal <sam.oneal@gmail.com>
2024-03-07 17:56:31 -0800

    Adds theme switcher.

--- myapp/urls.py
+++ myapp/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path("about", views.about, name="about"),
     path('function/', views.function_view, name='function_view'),
     path('class/', views.ClassView.as_view(), name='class_view'),
+    path('theme/', views.ThemeView.as_view(), name='theme'),
 ]
