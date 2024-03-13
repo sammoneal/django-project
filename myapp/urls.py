fd3ac74b79153d75f4e20b44c0aaf14098f66db4
Sam O'Neal <sam.oneal@gmail.com>
2024-03-12 22:43:19 -0700

    Adds inventions list/view and db loader utility. Improves home and about pages.

--- myapp/urls.py
+++ myapp/urls.py
@@ -2,9 +2,12 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.home, name="home"),
+    path("", views.HomeView.as_view(), name="home"),
     path("about", views.about, name="about"),
     path('function/', views.function_view, name='function_view'),
     path('class/', views.ClassView.as_view(), name='class_view'),
     path('theme/', views.ThemeView.as_view(), name='theme'),
+    path('load/', views.load_default_data_view, name='load_default_data'),
+    path('inventions/', views.InventionListView.as_view(), name='invention-list'),
+    path('invention/<int:pk>/', views.InventionDetailView.as_view(), name='invention-view')
 ]
